09901cc43a85bd5d2f731d2a1d9b55133a30ecfc
Joydeep Bhattacharjee <joydeepubuntu@gmail.com>
2017-03-15 12:02:58 +0530

    sample get request

---
+++ networking/get_request.hs
@@ -0,0 +1,12 @@
+import Network.HTTP.Client
+import Network.HTTP.Types.Status (statusCode)
+
+main :: IO()
+main = do
+  manager <- newManager defaultManagerSettings
+
+  request <- parseRequest "https://httpbin.org/get"
+  response <- httpLbs request manager
+
+  putStrLn $ "The status code was:" ++ (show $ statusCode $ responseStatus response)
+  print $ responseBody response
